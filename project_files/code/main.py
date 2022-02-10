2e660b73f4aa584c11a1eeadc955c08de35cb9a1
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2022-02-10 16:25:15 +0200

    Initial commit, functionality of reading from screen in real time

---
+++ project_files/code/main.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import win32gui
+from PIL import ImageGrab
+
+def capture_dynamic():
+    toplist, winlist = [], []
+
+    def enum_cb(hwnd, results):
+        winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+
+    win32gui.EnumWindows(enum_cb, toplist)
+
+    wnd = [(hwnd, title) for hwnd, title in winlist if '.mp4' in title.lower()]
+
+    if wnd:
+        wnd = wnd[0]
+        hwnd = wnd[0]
+
+        bbox = win32gui.GetWindowRect(hwnd)
+        img = ImageGrab.grab(bbox)
+        return img
+    else:
+        return None
+
+
+while(True):
+    #   Dynamic Version
+    img = capture_dynamic()
+
+    if(img == None):
+        print("No Window Found! Please Try Again")
+        break
+
+    img = np.array(img)
+
+    img = cv2.GaussianBlur(img, (9, 9), 0)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img = cv2.Canny(img, 100, 130)
+
+    kernel = np.ones((5, 5), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+
+    img = cv2.erode(img, kernel, iterations=1)
+
+    cv2.imshow('window', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    if cv2.waitKey(25) & 0xFF == ord('='):
+        new_img = np.zeros(img.shape, dtype='uint8')
+
+        con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(new_img, con, -1, (230, 111, 148), 1)
+
+        cv2.imshow("Result", new_img)
+        break
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
