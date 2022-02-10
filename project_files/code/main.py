9cf1ba9db7b0d683ed3ec1c0380ded296a8faabe
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2022-02-10 20:02:04 +0200

    Structured project, added time tracking for function, finding edges of objects and count objects

--- project_files/code/main.py
+++ project_files/code/main.py
@@ -11,7 +11,7 @@ def capture_dynamic():
 
     win32gui.EnumWindows(enum_cb, toplist)
 
-    wnd = [(hwnd, title) for hwnd, title in winlist if '.mp4' in title.lower()]
+    wnd = [(hwnd, title) for hwnd, title in winlist if 'firefox' in title.lower()]
 
     if wnd:
         wnd = wnd[0]
@@ -24,37 +24,59 @@ def capture_dynamic():
         return None
 
 
-while(True):
-    #   Dynamic Version
-    img = capture_dynamic()
-
-    if(img == None):
-        print("No Window Found! Please Try Again")
-        break
-
-    img = np.array(img)
-
-    img = cv2.GaussianBlur(img, (9, 9), 0)
+def get_frame(img):
+    # img = cv2.GaussianBlur(img, (9, 9), 0)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    img = cv2.Canny(img, 100, 130)
+    img = cv2.Canny(img, 50, 100)
 
     kernel = np.ones((5, 5), np.uint8)
     img = cv2.dilate(img, kernel, iterations=1)
 
     img = cv2.erode(img, kernel, iterations=1)
 
-    cv2.imshow('window', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    return cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    if cv2.waitKey(25) & 0xFF == ord('='):
-        new_img = np.zeros(img.shape, dtype='uint8')
 
-        con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-        cv2.drawContours(new_img, con, -1, (230, 111, 148), 1)
+@performance
+def process_frames():
+    i = 0
+    while True:
+        # #   Dynamic Version
+        # img = capture_dynamic()
+        #
+        # if(img == None):
+        #     print("No Window Found! Please Try Again")
+        #     break
+        #
+        # img = np.array(img)
+        i += 1
+        (success, img) = cap.read()
+        if not success:
+            print(i)
+            break
 
-        cv2.imshow("Result", new_img)
-        break
+        con, hir = get_frame(img)
 
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        # print(len(con))
+
+        # cv2.imshow('Result', img)
+
+        # if cv2.waitKey(25) & 0xFF == ord('='):
+        #     new_img = np.zeros(img.shape, dtype='uint8')
+        #
+        #     con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        #     cv2.drawContours(new_img, con, -1, (230, 111, 148), 1)
+        #
+        #     # print()
+        #
+        #     # cv2.imshow("Result", new_img)
+        #     break
+        #
+        # if cv2.waitKey(25) & 0xFF == ord('q'):
+        #     cv2.destroyAllWindows()
+        #     break
+
+
+if __name__ == "__main__":
+    process_frames()
